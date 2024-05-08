a2d447a4a619ae881f5776cb5650379483531401
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-08 20:30:22 +1200

    Created new class Player and implemented the basics of it

---
+++ src/main/java/nz/ac/auckland/se281/Player.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se281;
+
+public class Player {
+
+  private String playerName = null;
+
+  public Player(String name) {
+    playerName = name;
+  }
+
+  public String getPlayerName() {
+    return playerName;
+  }
+}
