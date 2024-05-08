8b185d259c24a1f0c3e96a0e429e3b60bee92767
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-08 16:15:22 +1200

    Added a new field in class Game to cound the rounds and implemented part of method play(). Passed T1_02

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,11 +6,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int roundCounter = 1;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    roundCounter = 1;
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundCounter));
+    
+    roundCounter++;
+  }
 
   public void endGame() {}
 
