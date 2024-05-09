528cfbc1cc1a2cb3d19b7cc22630155674fc93a8
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-10 11:31:05 +1200

    Added new field playerHighestParity and a setter for it

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -2,10 +2,16 @@ package nz.ac.auckland.se281;
 
 public class TopStrategy implements BotStrategies {
 
+  private int playerHighestParity = 0;
+
   public TopStrategy() {}
 
   public String pickNumber() {
 
     return "";
+  }
+
+  public void setPlayerHighestParity(int parity) {
+    playerHighestParity = parity;
   }
 }
