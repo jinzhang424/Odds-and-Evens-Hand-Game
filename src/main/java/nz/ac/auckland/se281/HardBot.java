765de2ece75ad6275715502744c5846a63b52a07
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-11 10:14:57 +1200

    Added method setStrategy and updated getStrategy

--- src/main/java/nz/ac/auckland/se281/HardBot.java
+++ src/main/java/nz/ac/auckland/se281/HardBot.java
@@ -9,6 +9,8 @@ public class HardBot implements BotDifficulty {
   private BotStrategies strategy = new RandomStrategy();
   private BotStrategies strategyRandom = new RandomStrategy();
   private BotStrategies strategyTop = new TopStrategy();
+  private int roundCounter = 0;
+  private boolean lostPreviousRound = false;
 
   public HardBot() {}
 
@@ -17,6 +19,20 @@ public class HardBot implements BotDifficulty {
   }
 
   public BotStrategies getStrategy() {
+    this.setStrategy();
     return strategy;
+  }
+
+  public void setStrategy() {
+    roundCounter++;
+
+    // When after round 3, toggles the strategy of the bot if it lost
+    if (roundCounter > 3 && lostPreviousRound) {
+      if (strategy.equals(strategyRandom)) {
+        strategy = strategyTop;
+      } else {
+        strategy = strategyRandom;
+      }
+    }
   }
 }
