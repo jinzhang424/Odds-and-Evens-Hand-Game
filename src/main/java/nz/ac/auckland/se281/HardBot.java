d34ac0a06dafca5f80b523b783731c49819342a1
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-11 10:24:20 +1200

    Updated the constructor for HardBot to also set the winCon for the TopStrategy and also delegating the initializing of strategies to it

--- src/main/java/nz/ac/auckland/se281/HardBot.java
+++ src/main/java/nz/ac/auckland/se281/HardBot.java
@@ -1,18 +1,26 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class HardBot implements BotDifficulty {
 
   private String botName = "HAL-9000";
   private Difficulty difficulty = Difficulty.HARD;
-  private BotStrategies strategy = new RandomStrategy();
+  private BotStrategies strategy = null;
   private BotStrategies strategyRandom = new RandomStrategy();
   private BotStrategies strategyTop = new TopStrategy();
   private int roundCounter = 0;
   private boolean lostPreviousRound = false;
 
-  public HardBot() {}
+  public HardBot(Choice winCon) {
+
+    strategy = new RandomStrategy();
+    strategyRandom = new RandomStrategy();
+    strategyTop = new TopStrategy();
+
+    strategyTop.setStrategyWinCon(winCon);
+  }
 
   public String getBotName() {
     return botName;
@@ -29,9 +37,11 @@ public class HardBot implements BotDifficulty {
     // When after round 3, toggles the strategy of the bot if it lost
     if (roundCounter > 3 && lostPreviousRound) {
       if (strategy.equals(strategyRandom)) {
+        System.out.println("SETTING STRATEGY TO TOP");
         strategy = strategyTop;
       } else {
         strategy = strategyRandom;
+        System.out.println("SETTING STRATEGY TO RANDOM");
       }
     }
   }
