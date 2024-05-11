f557deb43bb2e6345442e9d3473cbc8c836c0f69
Jin <ajhz011@aucklanduni.ac.nz>
2024-05-12 09:28:00 +1200

    Removed constructor for EasyBot as it was equivalent to the default constructor

--- src/main/java/nz/ac/auckland/se281/EasyBot.java
+++ src/main/java/nz/ac/auckland/se281/EasyBot.java
@@ -1,12 +1,9 @@
 package nz.ac.auckland.se281;
 
-
 public class EasyBot implements BotDifficulty {
 
   private String botName = "HAL-9000";
   private BotStrategies strategy = new RandomStrategy();
-
-  public EasyBot() {}
 
   public String getBotName() {
     return botName;
